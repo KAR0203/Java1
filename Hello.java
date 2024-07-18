082b320b705f7f5a79a7220c863b1dd7c75665f1
Karan Pandhare <114685767+KAR0203@users.noreply.github.com>
2024-07-18 12:06:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
-public static void main(String[]args){
-System.out.println("Hello Everyone!");
+public class Hello{
+  public static void main(String[]args){
+    System.out.println("Hello Wolrd");
 }
